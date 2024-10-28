24c1d68351240706fc332a0fabed1b2c5af8a952
Maciel Zeferino <maciel.zeferino0@gmail.com>
2024-10-28 09:06:51 -0300

    corretion bug the midware and routes

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require("cors");
+const app = express();
+
+require("./models/associations");
+
+const clientRoutes = require("./routes/clientRoutes");
+//const professionalRoutes = require("./routes/professionalRoutes");
+//const appointmentRoutes = require("./routes/appointmentRoutes")
+
+//const Professional = require("./models/professionals");
+//const Appointment = require("./models/appointments");
+const Client = require("./models/clients");
+
+app.use("/client", clientRoutes);
+//app.use("/professionals", professionalRoutes);
+//app.use("/appointments", appointmentRoutes);
+app.use(cors());
+app.use(express.json());
+
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=> console.log(`Online in port ${port}`))
